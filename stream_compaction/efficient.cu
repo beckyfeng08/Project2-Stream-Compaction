ee136ab0823473fd93b2e2254b501f20fb8f2bec
beckyfeng08 <beckyfeng08@berkeley.edu>
2026-09-13 21:38:41 -0400

    test scan in efficient

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -12,12 +12,68 @@ namespace StreamCompaction {
             return timer;
         }
 
+
+        __global__ void padzeros(int startidx, int endidx, int* a) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index < endidx - startidx) {
+                a[startidx + index] = 0;
+            }
+        }
+        __global__ void upsweep(int n, int offset, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index < n) {
+                int two_dplusone = 1 << (offset + 1);
+                int two_d = 1 << offset;
+
+                idata[index + two_dplusone - 1] += idata[index + two_d - 1];
+            }
+        }
+
+        __global__ void downsweep(int n, int offset, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+            if (index < n) {
+                int two_d = 1 << offset;
+                int two_dplusone = 1 << (offset + 1);
+
+                int t = idata[index + two_d - 1];
+                idata[index + two_d - 1] = idata[index + two_dplusone - 1];
+                idata[index + two_dplusone - 1] += t;
+            }
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+            // setting block size
+            int blockSize = 64;
+            int fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+            int* idata2;
+            int n_padded = n;
+
+            // check to see if we have a power of 2. If not, resize and pad with 0s
+            if (1 << ilog2ceil(n) != n) {
+                n_padded = 1 << ilog2ceil(n);
+            }
+
+            cudaMalloc((void**)&idata2, n_padded * sizeof(int));
+            cudaMemcpy(idata2, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            padzeros<<<fullBlocksPerGrid, blockSize>>>(n, n_padded, idata2);
+
+            // upsweep
+            for (int d = 0; d < ilog2ceil(n) - 1 ; d++) {
+                upsweep<<<fullBlocksPerGrid, blockSize>>>(n_padded, d, idata2);
+            }
+            // downsweep, geenrates exclusive scan
+            cudaMemset(idata2 + n_padded - 1, 0, sizeof(int));
+            for (int d = ilog2ceil(n) - 1; d > 0; d--) {
+                downsweep<<<fullBlocksPerGrid, blockSize>>>(n_padded, d, idata2);
+            }
+
+            cudaMemcpy(odata, idata2, n * sizeof(int), cudaMemcpyDeviceToHost);
+            cudaFree(idata2);
             timer().endGpuTimer();
         }
 
